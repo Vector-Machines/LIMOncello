ba706892555853f66b055b54677993ecebc048d0
Carlos Pérez <carlos.perez.ruiz@estudiantat.upc.edu>
2025-03-10 19:19:01 +0100

    Refactor done but plane to plane icp like GICP seems not a good idea for EKFs

--- src/main.cpp
+++ src/main.cpp
@@ -220,8 +220,7 @@ PROFC_NODE("LiDAR Callback")
     }
 
     // Update map
-    if (state_.stamp - first_imu_stamp_ < 58)
-      ioctree_.update(processed->points);
+    ioctree_.update(processed->points);
 
     if (cfg.verbose)
       PROFC_PRINT()
